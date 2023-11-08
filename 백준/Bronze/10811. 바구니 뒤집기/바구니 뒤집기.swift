b8d01c3cf541994dd0ac235fbb4496fa661dc579
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2023-11-09 01:27:51 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.swift
@@ -0,0 +1,22 @@
+let input = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
+var arr: [String] = {
+    var n: [String] = []
+    for i in 1...input[0] {
+        n.append(String(i))
+    }
+    return n
+}()
+
+func swap(from i: Int, to j: Int) {
+    for num in i...((i+j)/2) {
+        arr.swapAt(num-1, i+j-num-1)
+    }
+}
+
+for _ in 1...input[1] {
+    let r = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
+    swap(from: r[0], to: r[1])
+}
+
+let result = arr.joined(separator: " ")
+print(result)
