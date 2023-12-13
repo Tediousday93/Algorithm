e1e7c9adef1817f94e2187e8433e83022f4da323
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2023-12-13 21:55:43 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 296 ms, Memory: 77752 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.swift
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.swift
@@ -1,16 +1,20 @@
+var ans: String = ""
+
 func moveCount(n: Int) -> Int {
     return (1 << n) - 1
 }
+
 func hanoi(start: Int, end: Int, n: Int) {
     if n == 1 {
-        print("\(start) \(end)")
+        ans += "\(start) \(end)\n"
         return
     }
     hanoi(start: start, end: 6-start-end, n: n-1)
-    print("\(start) \(end)")
+    ans += "\(start) \(end)\n"
     hanoi(start: 6-start-end, end: end, n: n-1)
 }
 
 let k = Int(readLine()!)!
 print(moveCount(n: k))
 hanoi(start: 1, end: 3, n: k)
+print(ans)
