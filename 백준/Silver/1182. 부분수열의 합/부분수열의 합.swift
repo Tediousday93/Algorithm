1e0eeef837304d43d3234b4e0c2298b9ffeda8af
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2023-12-25 18:57:14 +0900

    [Silver II] Title: 부분수열의 합, Time: 36 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
@@ -0,0 +1,20 @@
+let ns = readLine()!.split(separator: " ").compactMap { Int($0) }
+let n = ns[0]
+let s = ns[1]
+var count: Int = 0
+var sum: Int = 0
+var arr: [Int] = readLine()!.split(separator: " ").compactMap { Int($0) }
+
+func solution(k: Int, sum: Int) {
+    if k == n {
+        if sum == s { count += 1 }
+        return
+    }
+    
+    solution(k: k+1, sum: sum)
+    solution(k: k+1, sum: sum + arr[k])
+}
+
+solution(k: 0, sum: 0)
+if s == 0 { count -= 1 }
+print(count)
