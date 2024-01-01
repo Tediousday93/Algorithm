58914656c9323c3ee710243e85d68ff39587d93c
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-01-01 21:40:30 +0900

    [Silver II] Title: N과 M (10), Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.swift
@@ -0,0 +1,26 @@
+let nm = readLine()!.split(separator: " ").compactMap { Int($0) }
+let n = nm[0]
+let m = nm[1]
+let list = readLine()!.split(separator: " ")
+    .compactMap { Int($0) }
+    .sorted(by: <)
+    .map { String($0) }
+var result = ""
+
+func solution(k: Int, cur: String, start: Int) {
+    if k == m {
+        result += cur + "\n"
+        return
+    }
+    
+    var tmp = ""
+    for i in start..<n {
+        if tmp != list[i] {
+            solution(k: k+1, cur: cur+list[i]+" ", start: i+1)
+            tmp = list[i]
+        }
+    }
+}
+
+solution(k: 0, cur: "", start: 0)
+print(result)
