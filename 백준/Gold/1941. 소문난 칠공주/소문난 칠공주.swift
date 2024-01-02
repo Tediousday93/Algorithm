aad58ce8ce1fd9f344d3c7dc801bb5f46bc08cae
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-01-02 22:40:37 +0900

    [Gold III] Title: 소문난 칠공주, Time: 204 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.swift
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.swift
@@ -1,5 +1,4 @@
 var students: [String] = []
-var board: [[String]] = []
 var result: Int = 0
 let dx: [Int] = [1, 0, -1, 0]
 let dy: [Int] = [0, 1, 0, -1]
@@ -7,61 +6,63 @@ let dy: [Int] = [0, 1, 0, -1]
 for _ in 1...5 {
     let list = readLine()!.map { String($0) }
     students += list
-    board.append(list)
 }
 
-func backTracking(k: Int, start: Int, indices: [(Int, Int)]) {
+func backTracking(k: Int, start: Int, indices: [Int]) {
     if k == 7 {
-        if checkSeat(indices: indices) { result += 1 }
+        if check(indices: indices) { result += 1 }
         return
     }
     
     for i in start..<25 {
-        backTracking(k: k+1, start: i+1, indices: indices + [(i/5, i%5)])
+        backTracking(k: k+1, start: i+1, indices: indices + [i])
     }
 }
 
-func checkSeat(indices: [(Int, Int)]) -> Bool {
-    var vis: [[Bool]] = .init(repeating: .init(repeating: false, count: 5), count: 5)
-    var stack: [(Int, Int)] = []
+func check(indices: [Int]) -> Bool {
+    var vis: [Bool] = .init(repeating: false, count: 7)
+    var stack: [Int] = []
+    var pointer: Int = 0
     let dx: [Int] = [1, 0, -1, 0]
     let dy: [Int] = [0, 1, 0, -1]
-    let firstIndex = indices[0]
-    var connected = 0
     var sCount = 0
     var yCount = 0
     
-    vis[firstIndex.0][firstIndex.1] = true
-    if board[firstIndex.0][firstIndex.1] == "S" {
+    vis[0] = true
+    if students[indices[0]] == "S" {
         sCount += 1
     } else {
         yCount += 1
     }
-    connected += 1
-    stack.append(firstIndex)
     
-    while let cur = stack.popLast() {
+    stack.append(indices[0])
+    
+    while pointer < stack.count {
+        let cur = stack[pointer]
+        pointer += 1
+        
         if yCount >= 4 { return false }
         
         for i in 0...3 {
-            let nx = cur.0 + dx[i]
-            let ny = cur.1 + dy[i]
+            let nx = cur / 5 + dx[i]
+            let ny = cur % 5 + dy[i]
             if !(0...4 ~= nx) || !(0...4 ~= ny) { continue }
-            if vis[nx][ny] { continue }
-            if indices.contains(where: { $0 == (nx, ny) }) {
-                if board[nx][ny] == "S" {
-                    sCount += 1
-                } else {
-                    yCount += 1
-                }
-                connected += 1
-                vis[nx][ny] = true
-                stack.append((nx, ny))
+            guard let idx = indices.firstIndex(of: nx * 5 + ny), !vis[idx] else {
+                continue
             }
+            
+            if students[nx * 5 + ny] == "S" {
+                sCount += 1
+            } else {
+                yCount += 1
+            }
+            
+            vis[idx] = true
+            stack.append(nx * 5 + ny)
         }
     }
     
-    return (connected == 7 ? true : false) && (sCount >= 4)
+    return (stack.count == 7) && (sCount >= 4)
 }
 
 backTracking(k: 0, start: 0, indices: [])
