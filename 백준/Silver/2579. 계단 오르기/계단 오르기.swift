c2382af7653b3bd8c73d661649f923447e31e560
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-01-15 02:22:37 +0900

    [Silver III] Title: 계단 오르기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.swift
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.swift
@@ -1,36 +1,21 @@
-func solution() {
-    let n = Int(readLine()!)!
-    var d: [[Int]] = .init(
-        repeating: .init(repeating: 0, count: 3),
-        count: n+1
-    )
-    
-    var points: [Int] = .init(repeating: 0, count: n+1)
-    for i in 1...n {
-        points[i] = Int(readLine()!)!
-    }
-    
-    d[1][1] = points[1]
-    
-    if n == 1 {
-        print(d[1][1])
-        return
-    }
-    
-    d[2][1] = points[2]
-    d[2][2] = points[1] + points[2]
-    
-    if n == 2 {
-        print(max(d[2][1], d[2][2]))
-        return
-    }
-    
-    for i in 3...n {
-        d[i][1] = max(d[i-2][1], d[i-2][2]) + points[i]
-        d[i][2] = d[i-1][1] + points[i]
-    }
-    
-    print(max(d[n][1], d[n][2]))
+let n = Int(readLine()!)!
+var d: [Int] = .init(repeating: 0, count: n+1)
+var arr: [Int] = []
+var point: Int = 0
+for i in 1...n {
+    arr.append(Int(readLine()!)!)
+    point += arr[i-1]
 }
 
-solution()
+if n > 3 {
+    d[1] = arr[0]
+    d[2] = arr[1]
+    d[3] = arr[2]
+    for i in 4...n {
+        d[i] = min(d[i-2], d[i-3]) + arr[i-1]
+    }
+    point -= min(d[n-1], d[n-2])
+} else if n == 3 {
+    point -= min(arr[0], arr[1])
+}
+print(point)
