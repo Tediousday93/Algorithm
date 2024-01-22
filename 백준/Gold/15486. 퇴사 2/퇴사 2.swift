edf72903d919d32f300babe34279da7dafabdbca
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-01-22 22:15:11 +0900

    [Gold V] Title: 퇴사 2, Time: 1916 ms, Memory: 114372 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.swift
@@ -0,0 +1,17 @@
+let n = Int(readLine()!)!
+var t: [Int] = []
+var p: [Int] = []
+for _ in 0..<n {
+    let TP = readLine()!.split(separator: " ").compactMap { Int($0) }
+    t.append(TP[0])
+    p.append(TP[1])
+}
+var d: [Int] = .init(repeating: 0, count: n+2)
+for i in stride(from: n, through: 1, by: -1) {
+    if i + t[i-1] <= n + 1 {
+        d[i] = max(d[i + t[i-1]] + p[i-1], d[i+1])
+    } else {
+        d[i] = d[i+1]
+    }
+}
+print(d.max()!)
