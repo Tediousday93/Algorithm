5e7aa932a5237eff586b652ee01fe379768f54e2
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-02-05 16:56:14 +0900

    [Gold V] Title: 동전 1, Time: 28 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.swift
@@ -0,0 +1,24 @@
+let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+let coinCount = input[0]
+let targetSum = input[1]
+
+var coinPoints: [Int] = []
+for _ in 0..<coinCount {
+    coinPoints.append(Int(readLine()!)!)
+}
+
+var dp: [Int] = .init(repeating: 0, count: targetSum+1)
+dp[0] = 1
+
+for pointIndex in 0..<coinCount {
+    let currentPoint = coinPoints[pointIndex]
+
+    for dpIndex in stride(from: currentPoint, through: targetSum, by: 1) {
+        guard Int.max - dp[dpIndex] - dp[dpIndex - currentPoint] > 0 else {
+            continue
+        }
+        dp[dpIndex] += dp[dpIndex - currentPoint]
+    }
+}
+
+print(dp[targetSum])
