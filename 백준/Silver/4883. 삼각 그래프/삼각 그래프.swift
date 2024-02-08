593c0a546df886432f2e1f323006385f14d54066
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-02-08 22:51:44 +0900

    [Silver I] Title: 삼각 그래프, Time: 180 ms, Memory: 76492 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.swift
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.swift
@@ -3,21 +3,18 @@ var testCount: Int = 0
 while let rowCount = Int(readLine()!), rowCount != 0 {
     testCount += 1
 
-    var graph: [[Int]] = [[]]
+    var dp: [[Int]] = [[0, 0, 0]]
     for _ in 0..<rowCount {
-        graph.append(readLine()!.split(separator: " ").compactMap({ Int($0) }))
+        dp.append(readLine()!.split(separator: " ").compactMap({ Int($0) }))
     }
 
-    var dp: [[Int]] = .init(repeating: [0, 0, 0], count: rowCount+1)
-    dp[1] = [graph[1][0], graph[1][1], (graph[1][1] + graph[1][2])]
-    dp[2][0] = dp[1][1] + graph[2][0]
-    dp[2][1] = min(dp[1][1], dp[1][2], dp[2][0]) + graph[2][1]
-    dp[2][2] = min(dp[1][1], dp[1][2], dp[2][1]) + graph[2][2]
+    dp[1][0] = Int.max
+    dp[1][2] += dp[1][1]
 
-    for row in 3..<rowCount+1 {
-        dp[row][0] = min(dp[row-1][0], dp[row-1][1]) + graph[row][0]
-        dp[row][1] = min(dp[row-1][0], dp[row-1][1], dp[row-1][2], dp[row][0]) + graph[row][1]
-        dp[row][2] = min(dp[row-1][1], dp[row-1][2], dp[row][1]) + graph[row][2]
+    for row in 2..<rowCount+1 {
+        dp[row][0] += min(dp[row-1][0], dp[row-1][1])
+        dp[row][1] += min(dp[row-1][0], dp[row-1][1], dp[row-1][2], dp[row][0])
+        dp[row][2] += min(dp[row-1][1], dp[row-1][2], dp[row][1])
     }
 
     print(String(testCount) + ". " + String(dp[rowCount][1]))
