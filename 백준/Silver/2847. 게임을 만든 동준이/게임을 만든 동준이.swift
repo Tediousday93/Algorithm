c0937d49327d9ab3105cac0a0e5d163cdcd6afb8
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-02-19 21:52:41 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 16 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.swift
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.swift
@@ -1,11 +1,10 @@
 let n = Int(readLine()!)!
 var count: Int = 0
-var clearPoints: [Int] = []
+var clearPoints: [Int] = .init(repeating: 0, count: n)
 
-for _ in 0..<n {
-    clearPoints.append(Int(readLine()!)!)
+for i in stride(from: n-1, through: 0, by: -1) {
+    clearPoints[i] = Int(readLine()!)!
 }
-clearPoints = clearPoints.reversed()
 
 for index in 1..<n {
     while clearPoints[index - 1] <= clearPoints[index] {
