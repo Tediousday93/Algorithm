23d071a3d2a9c20af7e86d5710045cc9dd045546
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-02-29 21:38:26 +0900

    [Silver II] Title: 베르트랑 공준, Time: 28 ms, Memory: 69344 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.swift
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.swift
@@ -1,23 +1,23 @@
-var isPrime: [Bool] = .init(repeating: true, count: 123456 * 2 + 1)
+let maximum = 123456
+var isPrime: [Bool] = .init(repeating: true, count: maximum * 2 + 1)
 isPrime[0] = false
 isPrime[1] = false
 
-while let input = readLine(), input != "0" {
-    let n = Int(input)!
-
-    var index: Int = 2
-    while index * index < 2*n {
-        if !isPrime[index] {
-            index += 1
-            continue
-        }
-
-        for i in stride(from: index * index, through: 2*n, by: index) {
-            isPrime[i] = false
-        }
+var index: Int = 2
+while index * index < maximum * 2 {
+    if !isPrime[index] {
         index += 1
+        continue
     }
 
+    for i in stride(from: index * index, through: maximum * 2, by: index) {
+        isPrime[i] = false
+    }
+    index += 1
+}
+
+while let input = readLine(), input != "0" {
+    let n = Int(input)!
     var primeCount: Int = 0
 
     (n+1...2*n).forEach {
