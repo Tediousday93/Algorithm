eb37e5ae9c4c92beaef55d425ea7840bc97b22ed
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-03-04 17:43:49 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.swift
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.swift
@@ -1,22 +1,2 @@
 let n = Int(readLine()!)!
-var multipleNum = n
-var fiveCount = 0
-
-while multipleNum != 0 {
-    let five = getCounts(n: multipleNum)
-    fiveCount += five
-    multipleNum -= 1
-}
-
-print(fiveCount)
-
-func getCounts(n: Int) -> Int {
-    var num = n
-    var five = 0
-    while num % 5 == 0 {
-        five += 1
-        num /= 5
-    }
-
-    return five
-}
+print(n/5 + n/25 + n/125)
