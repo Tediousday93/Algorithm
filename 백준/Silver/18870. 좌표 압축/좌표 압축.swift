f5898ca100aadeb5640400f7befcfa89a6cf4803
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-03-04 22:20:21 +0900

    [Silver II] Title: 좌표 압축, Time: 1060 ms, Memory: 144920 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.swift
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.swift
@@ -1,17 +1,30 @@
 let n = Int(readLine()!)!
-let input = readLine()!.split(separator: " ").compactMap { Int($0) }
-var dict: [Int: Int] = [:]
-var cur: Int = 0
-input.sorted(by: <)
-    .forEach {
-        if dict[$0] == nil {
-            dict[$0] = cur
-            cur += 1
-        }
+let targets = readLine()!.split(separator: " ")
+.compactMap { Int($0) }
+let points = targets.sorted()
+
+var unique: [Int] = [points[0]]
+for index in 1..<n {
+    if points[index] != points[index - 1] {
+        unique.append(points[index])
+    }
+}
+
+func lowerIndex(of x: Int) -> Int {
+    var start = 0
+    var end = unique.count
+    var mid: Int { (start + end) / 2 }
+
+    while start < end {
+        if unique[mid] >= x { end = mid }
+        else { start = mid + 1 }
     }
 
+    return start
+}
+
 var ans = ""
-input.forEach {
-    ans += String(dict[$0]!) + " "
+for target in targets {
+    ans += String(lowerIndex(of: target)) + " "
 }
 print(ans)
