baeabf17fdfaa7f14457839a7f39abe2c408db2b
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-03-05 09:52:29 +0900

    [Gold IV] Title: 세 수의 합, Time: 56 ms, Memory: 75368 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.swift
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.swift
@@ -6,20 +6,39 @@ func solution() {
     }
     numberSet.sort(by: >)
     
-    var twoElementSums: Set<Int> = []
+    var twoElementSums: [Int] = []
     for x in 0..<n {
         for y in x..<n {
-            twoElementSums.insert(numberSet[x] + numberSet[y])
+            twoElementSums.append(numberSet[x] + numberSet[y])
         }
     }
+    twoElementSums.sort()
     
     for number in numberSet {
         for j in stride(from: 0, to: n, by: 1) {
-            if twoElementSums.contains(number - numberSet[j]) {
+            if isContaining(number - numberSet[j], in: twoElementSums) {
                 print(number)
                 return
             }
         }
+    }
+    
+    func isContaining(_ x: Int, in arr: [Int]) -> Bool {
+        var start = 0
+        var end = arr.count - 1
+        var mid: Int { (start + end) / 2 }
+        
+        while start <= end {
+            if arr[mid] > x {
+                end = mid - 1
+            } else if arr[mid] < x {
+                start = mid + 1
+            } else {
+                return true
+            }
+        }
+        
+        return false
     }
 }
 solution()
