b2da5d5912cbc9b96d0e9f60daee7d141ed21427
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-04-09 11:49:15 +0900

    [level 2] Title: 문자열 압축, Time: 0.14 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.swift
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.swift
@@ -11,35 +11,20 @@ func solution(_ s:String) -> Int {
         
         for index in stride(from: 0, to: s.count, by: compressLength) {
             var endIndex = index + compressLength
-            var isLast = false
             
-            if endIndex >= alphabets.count {
-                endIndex = alphabets.count
-                isLast = true
-            }
+            if endIndex >= alphabets.count { endIndex = alphabets.count }
             
             let cur = alphabets[index..<endIndex].reduce("", +)
+            
             if cur == unit {
                 count += 1
             } else {
-                if count <= 1 {
-                    compressed += unit
-                } else {
-                    compressed += String(count) + unit
-                }
-                
+                compressed += ((count <= 1 ? "" : String(count)) + unit)
                 unit = cur
                 count = 1
             }
-            
-            if isLast {
-                if count == 1 {
-                    compressed += unit
-                } else {
-                    compressed += String(count) + unit
-                }
-            }
         }
+        compressed += ((count <= 1 ? "" : String(count)) + unit)
         
         minLength = min(minLength, compressed.count)
         compressed = ""
