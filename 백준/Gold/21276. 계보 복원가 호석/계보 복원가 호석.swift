72ae9936219804776a9c56316acff513d28a0b92
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-05-07 23:35:52 +0900

    [Gold III] Title: 계보 복원가 호석, Time: 808 ms, Memory: 74872 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.swift
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.swift
@@ -29,42 +29,42 @@ func solution() {
     var nameIndices: [String: Int] = [:]
     names.enumerated().forEach { nameIndices[$0.element] = $0.offset }
     
+    // input
     var tree: [[Int]] = .init(repeating: [], count: N)
     var indegree: [Int] = .init(repeating: 0, count: N)
-    var outdegree: [Int] = .init(repeating: 0, count: N)
     
     for _ in 0..<M {
         let input = readLine()!.split(separator: " ").map { String($0) }
         let childIndex = nameIndices[input[0]]!
         let parentIndex = nameIndices[input[1]]!
-        tree[childIndex].append(parentIndex)
-        indegree[parentIndex] += 1
-        outdegree[childIndex] += 1
+        tree[parentIndex].append(childIndex)
+        indegree[childIndex] += 1
     }
     
-    var queue: Queue<Int> = .init()
-    var children: [[Int]] = .init(repeating: [], count: N)
-    indegree.enumerated()
-        .filter { $0.element == 0 }
-        .forEach { queue.enqueue($0.offset) }
-    
-    while let cur = queue.dequeue() {
-        for parent in tree[cur] {
-            if outdegree[parent] == outdegree[cur] - 1 {
-                children[parent].append(cur)
-                indegree[parent] -= 1
-                if indegree[parent] == 0 { queue.enqueue(parent) }
-            }
-        }
-    }
-    
+    // results
     var K = 0
     var ancestorNames: [String] = []
     var informations: [String] = []
     
-    outdegree.enumerated()
+    var queue: Queue<Int> = .init()
+    var children: [[Int]] = .init(repeating: [], count: N)
+    
+    indegree.enumerated()
         .filter { $0.element == 0 }
-        .forEach { ancestorNames.append(names[$0.offset]) }
+        .forEach {
+            ancestorNames.append(names[$0.offset])
+            queue.enqueue($0.offset)
+        }
+    
+    while let cur = queue.dequeue() {
+        for child in tree[cur] {
+            if indegree[child] == 1 { children[cur].append(child) }
+            indegree[child] -= 1
+            if indegree[child] == 0 { queue.enqueue(child) }
+        }
+    }
+    
+    K = ancestorNames.count
     
     children.enumerated().forEach {
         let parentName = names[$0.offset]
@@ -77,8 +77,6 @@ func solution() {
         let info = "\(parentName) \(childrenCount) \(childrenNames.joined(separator: " "))"
         informations.append(info)
     }
-    
-    K = ancestorNames.count
     
     print("\(K)\n\(ancestorNames.joined(separator: " "))\n\(informations.joined(separator: "\n"))")
 }
