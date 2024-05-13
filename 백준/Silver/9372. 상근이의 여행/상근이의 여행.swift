7617e8dc2da0f765194f41f813552048f5a9fa7d
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-05-13 23:03:08 +0900

    [Silver IV] Title: 상근이의 여행, Time: 176 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.swift
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.swift
@@ -1,10 +1,8 @@
 let testCount = Int(readLine()!)!
 var result: [String] = []
-
 for _ in 0..<testCount {
     result.append(excuteTest())
 }
-
 print(result.joined(separator: "\n"))
 
 func excuteTest() -> String {
@@ -12,13 +10,12 @@ func excuteTest() -> String {
     let n = nm[0]
     let m = nm[1]
     var parents: [Int] = .init(0...n)
-    var rank: [Int] = .init(repeating: 0, count: n+1)
     var ans = 0
 
     for _ in 0..<m {
         let input = readLine()!.split(separator: " ").compactMap { Int($0) }
         if find(input[0], parents: &parents) != find(input[1], parents: &parents) {
-            union(input[0], input[1], parents: &parents, rank: &rank)
+            union(input[0], input[1], parents: &parents)
             ans += 1
         }
     }
@@ -32,18 +29,8 @@ func find(_ node: Int, parents: inout [Int]) -> Int {
     return parents[node]
 }
 
-func union(_ x: Int, _ y: Int, parents: inout [Int], rank: inout [Int]) {
+func union(_ x: Int, _ y: Int, parents: inout [Int]) {
     let rootX = find(x, parents: &parents)
     let rootY = find(y, parents: &parents)
-
-    if rootX == rootY { return }
-
-    if rank[rootX] < rank[rootY] {
-        parents[rootX] = rootY
-    } else {
-        parents[rootY] = rootX
-        if rank[rootX] == rank[rootY] {
-            rank[rootX] += 1
-        }
-    }
+    parents[rootY] = rootX
 }
