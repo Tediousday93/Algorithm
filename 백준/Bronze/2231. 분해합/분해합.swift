d52ac7b529d31022f341be434c1c7a39e5bba83e
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-07-17 02:46:06 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.swift
+++ 백준/Bronze/2231. 분해합/분해합.swift
@@ -1,13 +1,13 @@
 let input = readLine()!
 let n = Int(input)!, digitCount = input.count
-var minGenerator = 0
+var generator = 0
 
 for digitSum in 0...digitCount * 9 {
     let m = n - digitSum
-    let digitNumbers = String(m).compactMap { Int(String($0)) }
-    if n == m + digitNumbers.reduce(0, +) {
-        minGenerator = m
+    let digits = String(m).compactMap { Int(String($0)) }
+    if n == m + digits.reduce(0, +) {
+        generator = m
     }
 }
 
-print(minGenerator)
+print(generator)
