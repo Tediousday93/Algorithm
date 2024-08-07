455286470071ee394323ce9562547a7092d6579d
ROWAN <114981173+Tediousday93@users.noreply.github.com>
2024-08-07 16:18:00 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 60 ms, Memory: 70080 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.swift
@@ -0,0 +1,49 @@
+let t = Int(readLine()!)!
+let max = 1000000
+var ans = ""
+let isPrime = isPrimeArr()
+
+for _ in 0..<t {
+    let n = Int(readLine()!)!
+    ans += "\(partition(of: n))\n"
+}
+
+print(ans)
+
+func isPrimeArr() -> [Bool] {
+    var isPrime: [Bool] = .init(repeating: true, count: max + 5)
+    isPrime[0] = false
+    isPrime[1] = false
+
+    var index = 2
+    while index * index <= max + 5 {
+        if !isPrime[index] {
+            index += 1
+            continue
+        }
+
+        for multiple in stride(from: index * index, to: max + 5, by: index) {
+            isPrime[multiple] = false
+        }
+
+        index += 1
+    }
+
+    return isPrime
+}
+
+func partition(of num: Int) -> Int {
+    var count = 0
+    var curPrime = 2
+
+    while curPrime <= num / 2 {
+        if !isPrime[curPrime] {
+            curPrime += 1
+            continue
+        }
+        if isPrime[num - curPrime] { count += 1 }
+        curPrime += 1
+    }
+
+    return count
+}
